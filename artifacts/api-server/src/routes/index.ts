035288db8f29bb6844c0d3c9508b527bc22c0509
Khaledscorbionmm <khaledelmasry9700@gmail.com>
2026-05-30 14:28:11 +0300

    feat: register execute router in routes index

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -8,6 +8,7 @@ import usersRouter from "./users";
 import subscriptionsRouter from "./subscriptions";
 import leaderboardRouter from "./leaderboard";
 import adminRouter from "./admin";
+import executeRouter from "./execute";
 
 const router: IRouter = Router();
 
@@ -20,5 +21,6 @@ router.use(usersRouter);
 router.use(subscriptionsRouter);
 router.use(leaderboardRouter);
 router.use(adminRouter);
+router.use(executeRouter);
 
 export default router;
